7f7c6ec932653af7186a1cc2fc4e723837522117
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-02-23 02:55:33 -0300

    mobile

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,14 @@ const date = new Date();
 const year = date.getFullYear();
 
 document.getElementById("year").innerHTML = year
+
+var ul = document.querySelector('nav ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
+function menuShow() {
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open');
+    } else {
+        ul.classList.add('open');
+    }
+}
